fb37c8f68aec9e895d5f7d9f4d525849dd4df9c0
Eugal-git <eugeniatunha06@gmail.com>
2024-10-18 13:30:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "I shall imagine again",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
